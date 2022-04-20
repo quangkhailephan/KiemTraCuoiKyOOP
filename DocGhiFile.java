b37e7d0eb1767999e946dfd14ed886e955bceb70
quangkhailephan <96873485+quangkhailephan@users.noreply.github.com>
2022-04-20 22:50:31 +0700

    Create DocGhiFile.java

---
+++ DocGhiFile.java
@@ -0,0 +1,23 @@
+package ThiCuoiKy;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.List;
+
+class FileWriter {
+    public static void write(String fileName, List<HangHoa> listHangHoa) {
+    File file = new File(fileName);//File
+    PrintWriter out;
+    try {
+        out = new PrintWriter(file);
+        out.println(listHangHoa);
+        out.close();
+    } catch (FileNotFoundException e) {
+        System.out.println("Sai roi ");
+        e.printStackTrace();
+    }//Ghi vao File chuoi s
+   
+   
+    }
+}
